d2654efbb8d8cb7a882862b03f712517606d485f
tyhow99 <122315624+tyhow99@users.noreply.github.com>
2025-02-20 14:29:26 -0500

    Add temp table styling

--- event-management-system/src/components/Table.jsx
+++ event-management-system/src/components/Table.jsx
@@ -1,4 +1,4 @@
-
+import './Table.css';
 
 function Table(){
     return(
@@ -13,6 +13,8 @@ function Table(){
             <tbody>
                 <tr>
                     <th>7002</th>
+                    <th>Tyler</th>
+                    <th>Age</th>
                     <th>Tyler</th>
                     <th>Age</th>
                 </tr>
