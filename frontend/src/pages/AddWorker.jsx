fe924916ddd979655c99c120764ebe894ebea5a0
jack <122406208+jackmk1984@users.noreply.github.com>
2025-04-15 16:16:09 -0400

    Add AddWorker Page

---
+++ frontend/src/pages/AddWorker.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const AddWorker = () => {
+    
+    return (
+        <div>
+            <h1>Add Worker</h1>
+        </div>
+    );
+};
+
+export default AddWorker;
