5312e651b78dd58375b3ba4d6c73f52fec315470
sdFrasch <133533321+sdFrasch@users.noreply.github.com>
2025-04-29 22:19:40 -0400

    Fixed backend login so its easier for yall

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres"  ,
-    password: "Iwekeb48",
+    user: process.env.USER ||  "tyhow99" || "postgres"  ,
+    password: ""  || "Foxes2002$" || "Iwekeb48",
     host: "localhost",
     port: 5432,
     database: "event_management"
